7ee14849cf26f82c2041b82e776bad36bee18916
xian <iamchristianrusso@gmail.com>
2019-08-17 17:38:11 -0400

    work in progress

---
+++ app.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const fetch = require('node-fetch');
+const prompts = require('prompts');
+
+const url = 
--- index.js
+++ index.js
@@ -4,32 +4,15 @@ const fetch = require('node-fetch');
 const prompts = require('prompts');
 
 const url = "https://www.googleapis.com/books/v1/volumes?q="
-const author = "inauthor:"
 const title = "intitle:"
 const subject = "subject:"
 
 const questions = [
-    // {
-    //     type: 'toggle',
-    //     name: 'value',
-    //     message: 'Would you like to search by author?',
-    //     initial: true,
-    //     active: 'yes',
-    //     inactive: 'no',
-    //     // onRender(x) {
-    //     //     return {
-    //     //         type: 'text',
-    //     //         name: 'author',
-    //     //         message: 'What is the name of the author?',
-    //     //         validate: name => fetch(`${url}/${author}${name.split(" ").join("+")}`).then(res => res.json()).then(json => console.log(json)),
-    //     //     }
-    //     // },
-    // },
     {
         type: 'text',
         name: 'author',
-        message: 'What is the name of the author?',
-        validate: name => fetch(`${url}/inauthor:${name.split(" ").join("+")}&maxResults=1`)
+        message: 'Who is the author and what is the title of the book?',
+        validate: name => fetch(`${url}/inauthor:${name.split(" ").join("+")}&intitle:${name.split(" ").join("+")}&maxResults=1`)
                             .then(res => res.json())
                             .then(json => {
                                 let item = json.items[0];
@@ -37,12 +20,7 @@ const questions = [
                                 console.log(item.volumeInfo.authors);
                                 console.log(item.volumeInfo.description);
                             })
-                            ? true : false,
-    },
-    {
-        type: 'text',
-        name: 'title',
-        message: 'What is the title of the book?',
+                            ? true : console.log('BRR'),
     },
 ];
 
